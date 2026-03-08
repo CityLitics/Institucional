83d31c51c7fa934b9153b36ccc926e7774509670
rafaelP-calderon <rafael.calderon@sptech.school>
2026-03-08 17:10:12 -0300

    Validações completas V1.11

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -21,8 +21,75 @@ function gerarSenha() {
     }, 3000);
 }
 
+cnpj.addEventListener("input", () => {
+
+    let numeros = "";
+
+    for (let i = 0; i < cnpj.value.length; i++) {
+        if (!isNaN(cnpj.value[i]) && cnpj.value[i] !== " ") {
+            numeros += cnpj.value[i];
+        }
+    }
+
+    if (numeros.length > 14) {
+        numeros = numeros.substring(0, 14);
+    }
+
+    let formatado = "";
+
+    for (let i = 0; i < numeros.length; i++) {
+
+        formatado += numeros[i];
+
+        if (i === 1 || i === 4) {
+            formatado += ".";
+        }
+
+        if (i === 7) {
+            formatado += "/";
+        }
+
+        if (i === 11) {
+            formatado += "-";
+        }
+    }
+
+    cnpj.value = formatado;
+
+});
+
 function validarCampos (input, msg, tamanho) {
+    let dominios = [".com", ".br", ".net", ".org"];
+    let tipoEmail = ["@gmail", "@outlook", "@hotmail", "@yahoo"];
+    
     input.addEventListener('input', () => {
+        
+        if (input.id === "email") {
+            let dominioValido = false;
+            let tipoValido = false;
+
+            for (let i = 0; i < dominios.length; i++) {
+                if (email.value.includes(dominios[i])) {
+                    dominioValido = true;
+                }
+            }
+
+            for (let j = 0; j < tipoEmail.length; j++) {
+                if (email.value.includes(tipoEmail[j])) {
+                    tipoValido = true;
+                }
+            }
+
+        if ((!dominioValido || !tipoValido) || input.value.includes(" ")) {
+            msg.textContent = "O campo deve ser preenchido corretamente!";
+            msg.style.color = "#8B0000";
+            return;
+        }
+
+        msg.textContent = "Campo preenchido com sucesso!";
+        msg.style.color = "#90EE90";
+        }
+
         if (input.value.length < tamanho) {
             msg.textContent = "O campo deve ser preenchido corretamente!";
             msg.style.color = "#8B0000";
@@ -45,6 +112,11 @@ function validarCampos (input, msg, tamanho) {
 validarCampos(empresa, msgCorretoNome, 1);
 validarCampos(email, msgCorretoEmail, 7);
 validarCampos(cnpj, msgCorretoCnpj, 14);
+
+function cadastrar() {
+    sessionStorage.setItem("senha", senha.value);
+    sessionStorage.setItem("email", email.value);
+}
 
 
 
--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,9 @@
 const olhoAberto = document.querySelector("#olhoAberto");
 const olhoFechado = document.querySelector("#olhoFechado");
 const senha = document.querySelector("#senha");
+const email = document.querySelector("#email");
+
+let senhaSalva = sessionStorage.getItem("senha");
 
 olhoAberto.addEventListener("click", () => {
     senha.type = "text";
@@ -12,4 +15,42 @@ olhoFechado.addEventListener("click", () => {
     senha.type = "password";
     olhoFechado.style.display = "none";
     olhoAberto.style.display = "inline";
+});
+
+email.addEventListener("input", () => {
+    if (email.value.length >= 10 && email.value.includes("@") && (email.value.includes(".com") || email.value.includes(".br") || email.value.includes(".net") || email.value.includes(".org")) ) {
+        msgCorretoEmail.textContent = "Campo preenchido com sucesso!";
+        msgCorretoEmail.style.color = "#90EE90";
+
+        setTimeout(() => {
+            msgCorretoEmail.style.opacity = "0";
+        }, 2500);
+
+        setTimeout(() => {
+            msgCorretoEmail.style.opacity = "1";
+            msgCorretoEmail.textContent = "";
+        }, 3000);
+    } else {
+        msgCorretoEmail.textContent = "O campo deve ser preenchido corretamente!";
+        msgCorretoEmail.style.color = "#8B0000";
+    }
+});
+
+senha.addEventListener("input", () => {
+    if (senha.value === senhaSalva) {
+        msgCorretoSenha.textContent = "Campo preenchido com sucesso!";
+        msgCorretoSenha.style.color = "#90EE90";
+
+        setTimeout(() => {
+            msgCorretoSenha.style.opacity = "0";
+        }, 2500);
+
+        setTimeout(() => {
+            msgCorretoSenha.style.opacity = "1";
+            msgCorretoSenha.textContent = "";
+        }, 3000);
+    } else {
+        msgCorretoSenha.textContent = "O campo deve ser preenchido corretamente!";
+        msgCorretoSenha.style.color = "#8B0000";
+    }
 });
